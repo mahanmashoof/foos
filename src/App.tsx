b386b7f718c5c4c1e29506930288ae816c7ff937
Mahan Mashoof <mahan.mashoof@gmail.com>
2021-12-27 14:49:09 -0300

    read from firestore ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import { collection, getDocs } from "firebase/firestore";
+import db from "./firebase";
+
+const q = collection(db, "tournament");
+
+const getData = async() => {
+  const querySnapShot = await getDocs(q);
+  if (!querySnapShot.empty) {
+    querySnapShot.forEach((doc) => {
+      console.log(doc.data())
+    })
+  } else {
+    console.log("No such document!");
+  }
+}
 
 function App() {
   return (
     <div className="App">
       App component
+      <button onClick={getData}>get data</button>
     </div>
   );
 }
