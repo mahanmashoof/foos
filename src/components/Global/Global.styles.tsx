d927e65ead7c22ae9167bed934f389b49ca754e3
Mashoof <mmashoof@deloittedigital.se>
2022-03-16 11:16:43 +0100

    try font w https

--- src/components/Global/Global.styles.tsx
+++ src/components/Global/Global.styles.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const FontHeading = styled.div`
-  @import url(//db.onlinewebfonts.com/c/6068d986a7478d48d33799e3ac745ec2?family=Queen+of+Camelot);
+  @import url(https://db.onlinewebfonts.com/c/6068d986a7478d48d33799e3ac745ec2?family=Queen+of+Camelot);
   font-family: "Queen of Camelot";
   color: blue;
 `
