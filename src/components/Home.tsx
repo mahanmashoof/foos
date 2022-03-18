f044a281a0d5a60d62f5a55ca7f71e095242920f
Mashoof <mmashoof@deloittedigital.se>
2022-03-18 18:43:33 +0100

    home row 1 ok

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,18 +2,16 @@ import { Link } from "react-router-dom";
 import { Routes } from "../types/enums";
 import * as s from "./Home.styles";
 import Top from "./Top";
+import ReactPlayer from 'react-player'
 
 const Home = () => {
 
   const start = new Date('2022-04-11 09:00')
   const now = new Date()
   const diff = (start.getTime() - now.getTime()) / 1000
-  const daysLeft = Math.floor(diff / 60 / 60 / 24)
-  const hoursLeft = Math.floor(diff % (daysLeft * 3600) / 3600)
-  const minLeft = Math.floor(diff % (daysLeft * 3600) % (hoursLeft * 60) / 60)
-  console.log(daysLeft)
-  console.log(hoursLeft)
-  console.log(minLeft)
+  const daysLeft = diff > 0 ? Math.floor(diff / 60 / 60 / 24) : 0
+  const hoursLeft = diff > 0 ? Math.floor((diff - daysLeft * 24 * 3600) / 3600) : 0
+  const minLeft = diff > 0 ? Math.floor((diff - daysLeft * 24 * 3600 - hoursLeft * 3600) / 60) : 0
 
   return (
     <s.Main>
@@ -21,16 +19,18 @@ const Home = () => {
       <s.Content>
         <s.Row1>
           <s.VideoFrame>
-            <s.Video></s.Video>
+            <s.Video>
+              <ReactPlayer url='videos/foosIntro.mp4' width="100%" height="100%" controls={true} />
+            </s.Video>
           </s.VideoFrame>
           <s.FieldFrame>
             <s.Field>
               <s.CountDownHeading>Tournament starts in</s.CountDownHeading>
-              <s.CountDownDays>30</s.CountDownDays>
+              <s.CountDownDays>{daysLeft}</s.CountDownDays>
               <s.CountDownText>Days</s.CountDownText>
-              <s.CountDownHours>10</s.CountDownHours>
+              <s.CountDownHours>{hoursLeft}</s.CountDownHours>
               <s.CountDownText>Hours</s.CountDownText>
-              <s.CountDownMin>20</s.CountDownMin>
+              <s.CountDownMin>{minLeft}</s.CountDownMin>
               <s.CountDownText>Minutes</s.CountDownText>
             </s.Field>
           </s.FieldFrame>
