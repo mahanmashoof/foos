3a27fe1c0417328cc624c4285841b91f838acc37
Mashoof <mmashoof@deloittedigital.se>
2022-04-05 15:41:10 +0200

    auto-creating group stage games ok

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -1,8 +1,8 @@
 import db from "../../firebase";
 import { setDoc, doc, deleteDoc, collection, addDoc } from "firebase/firestore";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { countriesDropdown, groupsDropdown, playersDropdown } from "../../helpers/Dropdown";
-import { useGetGames, useGetTeams } from "../../hooks/api/useApi";
+import { useGetTeams } from "../../hooks/api/useApi";
 import { APITeam } from "../../models/api/APIGroups";
 import { GroupInfo } from "../../types/types";
 import { GameStatus } from "../../types/enums";
@@ -12,14 +12,11 @@ const Admin = () => {
 
   const [name, setName] = useState("");
   const [flagUrl, setFlagUrl] = useState("");
-  const [flagUrl2, setFlagUrl2] = useState("");
   const [player1, setPlayer1] = useState('');
   const [player1Card, setPlayer1Card] = useState('');
   const [player2, setPlayer2] = useState('');
   const [player2Card, setPlayer2Card] = useState('');
   const [group, setGroup] = useState('');
-  const [team1Name, setTeam1Name] = useState('');
-  const [team2Name, setTeam2Name] = useState('');
 
   //--- Add a team
   const addTeam = async () => {
@@ -71,34 +68,7 @@ const Admin = () => {
   }
   // ---
 
-  /*   //--- Add a game
-    const addGame = async () => {
-      await addDoc(collection(db, "games"), {
-        group: group,
-        status: GameStatus.NOT_PLAYED,
-        team1: {
-          name: team1Name,
-          flagUrl: flagUrl,
-          goals: 0,
-        },
-        team2: {
-          name: team2Name,
-          flagUrl: flagUrl2,
-          goals: 0,
-        },
-      })
-        .then(() => {
-          alert(`${team1Name} - ${team2Name} added`);
-        })
-        .catch((err) => console.log(err));
-    };
-    // --- */
-
-  const [separatedGroup, setSeparatedGroup] = useState<string[]>([])
-
-  useEffect(() => {
-    setSeparatedGroup(teams.filter(team => team.group === group).map(team => team.name))
-  }, [teams, group])
+  const separatedGroup = teams.filter(team => team.group === group).map(team => team.name)
 
   let gameCreationArray = [[0, 0]]
   switch (separatedGroup.length) {
@@ -117,11 +87,9 @@ const Admin = () => {
     [separatedGroup[combo[0]], separatedGroup[combo[1]]]
   ))
 
-  const gameId = teams.length.toString()
-
   //--- Add a game
   const addGame = async (teamName1: string, teamName2: string) => {
-    await setDoc(doc(db, "games", gameId), {
+    await addDoc(collection(db, "games"), {
       group: group,
       status: GameStatus.NOT_PLAYED,
       team1: {
@@ -204,46 +172,19 @@ const Admin = () => {
             </div>
           )}
         </div>}
-      <div>
-        <label htmlFor="group">Choose group:</label>
-        <select id="group" onChange={(e) => setGroup(e.target.value)}>
-          {groupsDropdown.map((option, i) => (
-            <option key={i} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-        <button onClick={() => createGames()}>create group games</button>
-      </div>
-
-      <div id="create-game">
-        <h3 style={{ marginTop: '3rem' }}>2) Add group stage games by choosing Group, Home team, and Away team</h3>
-        <label htmlFor="group">Group:</label>
-        <select id="group" onChange={(e) => setGroup(e.target.value)}>
-          {groupsDropdown.map((option, i) => (
-            <option key={i} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-        <label htmlFor="team1">Home team:</label>
-        <select id="team1" onChange={(e) => { setTeam1Name(e.target.value); setFlagUrl(`flags/${e.target.value}.png`) }}>
-          {countriesDropdown.map((option, i) => (
-            <option key={i} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-        <label htmlFor="team2">Away team:</label>
-        <select id="team2" onChange={(e) => { setTeam2Name(e.target.value); setFlagUrl2(`flags/${e.target.value}.png`) }}>
-          {countriesDropdown.map((option, i) => (
-            <option key={i} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-        <button onClick={() => console.log('apply addGame function  if auto mode fails')}>add game</button>
-      </div>
+      {teams.length > 0 &&
+        <div>
+          <h3 style={{ marginTop: '3rem' }}>2) When finished setting the groups, add teams to the tournament by selecting Group</h3>
+          <label htmlFor="group"></label>
+          <select id="group" onChange={(e) => setGroup(e.target.value)}>
+            {groupsDropdown.map((option, i) => (
+              <option key={i} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          <button onClick={() => createGames()}>create group games</button>
+        </div>}
     </div>
   );
 };
