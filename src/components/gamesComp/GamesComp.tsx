b0f39f150183cab408b6c69bf71a4dcb56bedb67
Mashoof <mmashoof@deloittedigital.se>
2022-04-07 13:25:24 +0200

    vertical table alignment ok

--- src/components/gamesComp/GamesComp.tsx
+++ src/components/gamesComp/GamesComp.tsx
@@ -12,14 +12,14 @@ interface GameResult {
 const GamesComp = (props: GameResult) => {
     return (
         <s.Main>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ display: 'flex', alignItems: 'center', width: '30%', justifyContent: 'start' }}>
                 <s.Flag alt='...' src={props.homeFlagUrl} />
                 <s.Text>{props.homeTeam}</s.Text>
             </div>
             <div>
                 <s.Text>{props.homeScore} - {props.awayScore}</s.Text>
             </div>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ display: 'flex', alignItems: 'center', width: '30%', justifyContent: 'end' }}>
                 <s.Text>{props.awayTeam}</s.Text>
                 <s.Flag alt='...' src={props.awayFlagUrl} />
             </div>
