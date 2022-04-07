95e60ed47f8acf3179e5e897a551d0f073b3ac9c
Mashoof <mmashoof@deloittedigital.se>
2022-04-07 13:31:36 +0200

    team name formats ok

--- src/components/gamesComp/GamesComp.tsx
+++ src/components/gamesComp/GamesComp.tsx
@@ -10,17 +10,25 @@ interface GameResult {
 }
 
 const GamesComp = (props: GameResult) => {
+    const adjustTeamName = (team: string) => {
+        if (team === 'usa') {
+            return team.toUpperCase()
+        } else {
+            return team[0].toUpperCase() + team.slice(1)
+        }
+    }
+
     return (
         <s.Main>
             <div style={{ display: 'flex', alignItems: 'center', width: '30%', justifyContent: 'start' }}>
                 <s.Flag alt='...' src={props.homeFlagUrl} />
-                <s.Text>{props.homeTeam}</s.Text>
+                <s.Text>{adjustTeamName(props.homeTeam)}</s.Text>
             </div>
             <div>
                 <s.Text>{props.homeScore} - {props.awayScore}</s.Text>
             </div>
             <div style={{ display: 'flex', alignItems: 'center', width: '30%', justifyContent: 'end' }}>
-                <s.Text>{props.awayTeam}</s.Text>
+                <s.Text>{adjustTeamName(props.awayTeam)}</s.Text>
                 <s.Flag alt='...' src={props.awayFlagUrl} />
             </div>
         </s.Main>
